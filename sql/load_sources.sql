cab74308e2f84ebadc2c7d458599f4c3599f995f
Damon McCullough <damonmcc1391@gmail.com>
2024-03-15 12:10:47 -0400

    rename source data tables to use prefix `source_`

--- sql/load_sources.sql
+++ sql/load_sources.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS pluto;
+DROP TABLE IF EXISTS source_pluto;
 DROP INDEX IF EXISTS pluto_geom_idx;
-CREATE TABLE IF NOT EXISTS "pluto" (
+CREATE TABLE IF NOT EXISTS "source_pluto" (
 	"bbl" text PRIMARY KEY NOT NULL,
 	"borough" char(2) NOT NULL,
 	"block" text NOT NULL,
@@ -10,22 +10,22 @@ CREATE TABLE IF NOT EXISTS "pluto" (
 	"wkt" geometry
 );
 CREATE INDEX pluto_geom_idx
-  ON pluto
+  ON source_pluto
   USING GIST (wkt);
 
-\COPY pluto ("wkt", "borough", "block", "lot", "address", "land_use", "bbl") FROM './pluto.csv' DELIMITER ',' CSV HEADER;
+\COPY source_pluto ("wkt", "borough", "block", "lot", "address", "land_use", "bbl") FROM './pluto.csv' DELIMITER ',' CSV HEADER;
 
 
-DROP TABLE IF EXISTS zoning_districts;
+DROP TABLE IF EXISTS source_zoning_districts;
 DROP INDEX IF EXISTS zoning_districts_geom_idx;
-CREATE TABLE IF NOT EXISTS "zoning_districts" (
+CREATE TABLE IF NOT EXISTS "source_zoning_districts" (
 	"zonedist" text NOT NULL,
 	"shape_leng" float,
 	"shape_area" float,
 	"wkt" geometry
 );
 CREATE INDEX zoning_districts_geom_idx
-  ON zoning_districts
+  ON source_zoning_districts
   USING GIST (wkt);
 
-\COPY zoning_districts ("wkt", "zonedist", "shape_leng", "shape_area") FROM './zoning_districts.csv' DELIMITER ',' CSV HEADER;
+\COPY source_zoning_districts ("wkt", "zonedist", "shape_leng", "shape_area") FROM './zoning_districts.csv' DELIMITER ',' CSV HEADER;
