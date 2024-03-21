5444dbc575b456f081343208945bd9d149b9c4e5
Damon McCullough <damonmcc1391@gmail.com>
2024-03-21 15:35:43 -0400

    download source data to a directory
    a directory which is ignored by git

--- bash/download.sh
+++ bash/download.sh
@@ -7,6 +7,10 @@ set -e
 export $(cat .env | sed 's/#.*//g' | xargs)
 
 # Download CSV files from Digital Ocean file storage
-mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/pluto.csv pluto.csv
-mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/zoning_districts.csv zoning_districts.csv
-mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/source_data_versions.csv source_data_versions.csv
+DATA_DIRECTORY=.data/
+mkdir -p ${DATA_DIRECTORY} && (
+    cd ${DATA_DIRECTORY}
+    mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/pluto.csv pluto.csv
+    mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/zoning_districts.csv zoning_districts.csv
+    mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/source_data_versions.csv source_data_versions.csv
+)
