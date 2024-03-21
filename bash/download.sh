3e536cb169da7108a134535d7efaaf34cf669184
Damon McCullough <damonmcc1391@gmail.com>
2024-03-21 13:53:31 -0400

    split bash script into three and update readme

---
+++ bash/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -e
+
+# To use environment variables defined in .env:
+export $(cat .env | sed 's/#.*//g' | xargs)
+
+# Download CSV files from Digital Ocean file storage
+mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/pluto.csv pluto.csv
+mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/zoning_districts.csv zoning_districts.csv
+mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/source_data_versions.csv source_data_versions.csv
