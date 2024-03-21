9388df81e382800ef6f03d011cd2608d81a99498
Damon McCullough <damonmcc1391@gmail.com>
2024-03-21 15:38:25 -0400

    make `create_tables.sql` easier to re-run
    before creating tables, delete them if they exists

--- create_tables.sql
+++ create_tables.sql
@@ -1,17 +1,20 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 
+DROP TABLE IF EXISTS borough CASCADE;
 CREATE TABLE IF NOT EXISTS "borough" (
 	"id" char(1) PRIMARY KEY NOT NULL,
 	"title" text NOT NULL,
 	"abbr" text NOT NULL
 );
 
+DROP TABLE IF EXISTS land_use CASCADE;
 CREATE TABLE IF NOT EXISTS "land_use" (
 	"id" char(2) PRIMARY KEY NOT NULL,
 	"description" text NOT NULL,
 	"color" char(9) NOT NULL
 );
 
+DROP TABLE IF EXISTS tax_lot CASCADE;
 CREATE TABLE IF NOT EXISTS "tax_lot" (
 	"bbl" char(10) PRIMARY KEY NOT NULL,
 	"borough_id" char(1) NOT NULL,
@@ -22,13 +25,13 @@ CREATE TABLE IF NOT EXISTS "tax_lot" (
 	"wgs84" geography(multiPolygon, 4326) NOT NULL,
 	"li_ft" geometry(multiPolygon,2263) NOT NULL
 );
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "tax_lot" ADD CONSTRAINT "tax_lot_borough_id_borough_id_fk" FOREIGN KEY ("borough_id") REFERENCES "borough"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "tax_lot" ADD CONSTRAINT "tax_lot_land_use_id_land_use_id_fk" FOREIGN KEY ("land_use_id") REFERENCES "land_use"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
@@ -41,14 +44,16 @@ DO $$ BEGIN
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
+DROP TABLE IF EXISTS zoning_district CASCADE;
 CREATE TABLE IF NOT EXISTS "zoning_district" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"label" text NOT NULL,
 	"wgs84" geography(multiPolygon, 4326) NOT NULL,
 	"li_ft" geometry(multiPolygon,2263) NOT NULL
 );
---> statement-breakpoint
+
+DROP TABLE IF EXISTS zoning_district_class CASCADE;
 CREATE TABLE IF NOT EXISTS "zoning_district_class" (
 	"id" text PRIMARY KEY NOT NULL,
 	"category" "category",
@@ -56,18 +61,19 @@ CREATE TABLE IF NOT EXISTS "zoning_district_class" (
 	"url" text,
 	"color" char(9) NOT NULL
 );
---> statement-breakpoint
+
+DROP TABLE IF EXISTS zoning_district_zoning_district_class CASCADE;
 CREATE TABLE IF NOT EXISTS "zoning_district_zoning_district_class" (
 	"zoning_district_id" uuid NOT NULL,
 	"zoning_district_class_id" text NOT NULL
 );
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "zoning_district_zoning_district_class" ADD CONSTRAINT "zoning_district_zoning_district_class_zoning_district_id_zoning_district_id_fk" FOREIGN KEY ("zoning_district_id") REFERENCES "zoning_district"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
  ALTER TABLE "zoning_district_zoning_district_class" ADD CONSTRAINT "zoning_district_zoning_district_class_zoning_district_class_id_zoning_district_class_id_fk" FOREIGN KEY ("zoning_district_class_id") REFERENCES "zoning_district_class"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
