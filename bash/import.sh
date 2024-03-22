12b0f4f19660a0f2e6b742cbe2344d12f14d6bc2
Damon McCullough <damonmcc1391@gmail.com>
2024-03-22 16:23:23 -0400

    set database uri envars everywhere they're needed

--- bash/import.sh
+++ bash/import.sh
@@ -5,13 +5,13 @@ set -e
 
 # To use environment variables defined in .env:
 export $(cat .env | sed 's/#.*//g' | xargs)
+export BUILD_ENGINE_SERVER=postgresql://${BUILD_ENGINE_USER}:${BUILD_ENGINE_PASSWORD}@${BUILD_ENGINE_HOST}:${BUILD_ENGINE_PORT}
+export BUILD_ENGINE_URI=${BUILD_ENGINE_SERVER}/${BUILD_ENGINE_DB}
 
 # Test database connection
 dbt debug
 
 # Copy CSV files into source data tables
-export BUILD_ENGINE_SERVER=postgresql://${BUILD_ENGINE_USER}:${BUILD_ENGINE_PASSWORD}@${BUILD_ENGINE_HOST}:${BUILD_ENGINE_PORT}
-export BUILD_ENGINE_URI=${BUILD_ENGINE_SERVER}/${BUILD_ENGINE_DB}
 
 psql ${BUILD_ENGINE_URI} \
   --set ON_ERROR_STOP=1 --single-transaction --quiet \
--- bash/transform.sh
+++ bash/transform.sh
@@ -5,6 +5,8 @@ set -e
 
 # To use environment variables defined in .env:
 export $(cat .env | sed 's/#.*//g' | xargs)
+export BUILD_ENGINE_SERVER=postgresql://${BUILD_ENGINE_USER}:${BUILD_ENGINE_PASSWORD}@${BUILD_ENGINE_HOST}:${BUILD_ENGINE_PORT}
+export BUILD_ENGINE_URI=${BUILD_ENGINE_SERVER}/${BUILD_ENGINE_DB}
 
 # Test database connection
 dbt debug
