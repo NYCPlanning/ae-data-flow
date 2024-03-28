35ce18e63db35820a68d58ea70e563584a9084fe
Damon McCullough <damonmcc1391@gmail.com>
2024-03-28 10:59:55 -0400

    set data flow envars in `export.sh`

--- bash/export.sh
+++ bash/export.sh
@@ -5,6 +5,8 @@ set -e
 
 # To use environment variables defined in .env:
 export $(cat .env | sed 's/#.*//g' | xargs)
+export BUILD_ENGINE_SERVER=postgresql://${BUILD_ENGINE_USER}:${BUILD_ENGINE_PASSWORD}@${BUILD_ENGINE_HOST}:${BUILD_ENGINE_PORT}
+export BUILD_ENGINE_URI=${BUILD_ENGINE_SERVER}/${BUILD_ENGINE_DB}
 
 export BUILD_ZONING_API_DB_SERVER=postgresql://${ZONING_API_USER}:${ZONING_API_PASSWORD}@${ZONING_API_HOST}:${ZONING_API_PORT}
 export BUILD_ZONING_API_ENGINE_URI=${BUILD_ZONING_API_DB_SERVER}/${ZONING_API_DB}
