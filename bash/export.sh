29a5108ce333a897cfaf2e41b2bfc5c63674fec9
Damon McCullough <damonmcc1391@gmail.com>
2024-03-28 21:13:22 -0400

    avoid `ALTER OWNER` statements from dump file

--- bash/export.sh
+++ bash/export.sh
@@ -12,7 +12,7 @@ source $ROOT_DIR/bash/utils/set_environment_variables.sh
 set_envars
 
 # Dump Zoning API DB tables to db.dump
-pg_dump -Fc -t tax_lot -t borough -t land_use -t zoning_district -t zoning_district_class -t zoning_district_zoning_district_class ${BUILD_ENGINE_URI} > db.dump
+pg_dump -Fc --no-owner -t tax_lot -t borough -t land_use -t zoning_district -t zoning_district_class -t zoning_district_zoning_district_class ${BUILD_ENGINE_URI} > db.dump
 
 # Restore Zoning API DB with updated data from db.dump
 pg_restore --clean --if-exists -d ${BUILD_ZONING_API_ENGINE_URI} db.dump
