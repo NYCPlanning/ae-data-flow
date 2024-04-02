d00143b5954b54261cc712272c2e69dc45e3b7e6
Damon McCullough <damonmcc1391@gmail.com>
2024-04-02 10:59:50 -0400

    use the data directory for db.dump file

--- bash/export.sh
+++ bash/export.sh
@@ -6,13 +6,18 @@ set -e
 FILE_DIR=$(dirname "$(readlink -f "$0")")
 ROOT_DIR=$FILE_DIR/../
 
+DATA_DIRECTORY=.data/
+
 source $ROOT_DIR/bash/utils/set_environment_variables.sh
 
 # Setting Environmental Variables
 set_envars
 
-# Dump Zoning API DB tables to db.dump
-pg_dump -Fc --no-owner -t tax_lot -t borough -t land_use -t zoning_district -t zoning_district_class -t zoning_district_zoning_district_class ${BUILD_ENGINE_URI} > db.dump
+mkdir -p ${DATA_DIRECTORY} && (
+    cd ${DATA_DIRECTORY}
+    # Dump Zoning API DB tables to db.dump
+    pg_dump -Fc --no-owner -t tax_lot -t borough -t land_use -t zoning_district -t zoning_district_class -t zoning_district_zoning_district_class ${BUILD_ENGINE_URI} > db.dump
 
-# Restore Zoning API DB with updated data from db.dump
-pg_restore --no-owner --clean --if-exists -d ${BUILD_ZONING_API_ENGINE_URI} db.dump
+    # Restore Zoning API DB with updated data from db.dump
+    pg_restore --no-owner --clean --if-exists -d ${BUILD_ZONING_API_ENGINE_URI} db.dump
+)
