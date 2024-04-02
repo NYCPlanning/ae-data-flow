92d27a6d79eb4ff2e35f7dec571d250077a9d177
Damon McCullough <damonmcc1391@gmail.com>
2024-04-02 13:54:28 -0400

    fix typo in utils

--- bash/utils/set_environment_variables.sh
+++ bash/utils/set_environment_variables.sh
@@ -8,10 +8,10 @@ ROOT_DIR=$(dirname ${UTILS_DIR})/../
 
 function set_envars {
     # To use environment variables defined in .env:
-    echo "settting environment variables from .env file ..."
+    echo "setting environment variables from .env file ..."
     export $(cat $ROOT_DIR/.env | sed 's/#.*//g' | xargs)
 
-    echo "settting other environment variables ..."
+    echo "setting other environment variables ..."
     export BUILD_ENGINE_SERVER=postgresql://${BUILD_ENGINE_USER}:${BUILD_ENGINE_PASSWORD}@${BUILD_ENGINE_HOST}:${BUILD_ENGINE_PORT}
     export BUILD_ENGINE_URI=${BUILD_ENGINE_SERVER}/${BUILD_ENGINE_DB}
 
