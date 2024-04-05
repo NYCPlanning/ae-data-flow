08cc6cf06c15137eb07edca59a91fb065f19e64d
tangoyankee <miller.tim108@gmail.com>
2024-04-05 17:15:39 -0400

    remove slashes from mcw

--- bash/download.sh
+++ bash/download.sh
@@ -12,13 +12,13 @@ source $ROOT_DIR/bash/utils/set_environment_variables.sh
 set_envars
 
 # set alias
-/mc alias set spaces $DO_SPACES_ENDPOINT $DO_SPACES_ACCESS_KEY $DO_SPACES_SECRET_KEY
+mc alias set spaces $DO_SPACES_ENDPOINT $DO_SPACES_ACCESS_KEY $DO_SPACES_SECRET_KEY
 
 # Download CSV files from Digital Ocean file storage
 DATA_DIRECTORY=.data/
 mkdir -p ${DATA_DIRECTORY} && (
     cd ${DATA_DIRECTORY}
-    /mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/pluto.csv pluto.csv
-    /mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/zoning_districts.csv zoning_districts.csv
-    /mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/source_data_versions.csv source_data_versions.csv
+    mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/pluto.csv pluto.csv
+    mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/zoning_districts.csv zoning_districts.csv
+    mc cp spaces/${DO_SPACES_BUCKET_DISTRIBUTIONS}/dcp_pluto/23v3/attachments/source_data_versions.csv source_data_versions.csv
 )
