422390d6f34304816314d58dc1f63dbd6c5d2d7b
tangoyankee <miller.tim108@gmail.com>
2024-07-11 15:51:50 -0400

    load spatial projects and zd classes

--- pg/copy.ts
+++ pg/copy.ts
@@ -49,22 +49,124 @@ import { pgPool } from "./pg-connector";
             fileName: "cpdb_planned_commitments.csv"
         },
         {
+            table: "source_capital_project",
+            columns: [
+                "ccp_version",
+                "m_a_proj_id",
+                "m_agency_acro",
+                "m_agency" ,
+                "m_agency_name" ,
+                "description" ,
+                "proj_id" ,
+                "min_date",
+                "max_date",
+                "type_category" ,
+                "plannedcommit_ccnonexempt" ,
+                "plannedcommit_ccexempt" ,
+                "plannedcommit_citycost" ,
+                "plannedcommit_nccstate" ,
+                "plannedcommit_nccfederal" ,
+                "plannedcommit_nccother" ,
+                "plannedcommit_noncitycost" ,
+                "plannedcommit_total" ,
+                "adopt_ccnonexempt" ,
+                "adopt_ccexempt" ,
+                "adopt_citycost" ,
+                "adopt_nccstate" ,
+                "adopt_nccfederal" ,
+                "adopt_nccother" ,
+                "adopt_noncitycost" ,
+                "adopt_total" ,
+                "allocate_ccnonexempt" ,
+                "allocate_ccexempt" ,
+                "allocate_citycost" ,
+                "allocate_nccstate" ,
+                "allocate_nccfederal" ,
+                "allocate_nccother" ,
+                "allocate_noncitycost" ,
+                "allocate_total" ,
+                "commit_ccnonexempt" ,
+                "commit_ccexempt" ,
+                "commit_citycost" ,
+                "commit_nccstate" ,
+                "commit_nccfederal" ,
+                "commit_nccother" ,
+                "commit_noncitycost" ,
+                "commit_total" ,
+                "spent_ccnonexempt" ,
+                "spent_ccexempt" ,
+                "spent_citycost" ,
+                "spent_nccstate" ,
+                "spent_nccfederal" ,
+                "spent_nccother" ,
+                "spent_noncitycost" ,
+                "spent_total" ,
+                "spent_total_checkbooknyc" 
+            ],
+            filePath: "data/download",
+            fileName: "cpdb_projects.csv" 
+        },
+        {
+            table: "source_capital_project_poly",
+            columns: [
+                "ccpversion",
+                "maprojid",
+                "magency",
+                "magencyacr",
+                "projectid",
+                "descriptio",
+                "typecatego",
+                "geomsource",
+                "dataname",
+                "datasource",
+                "datadate",
+                "wkt"
+            ],
+            filePath: "data/convert",
+            fileName: "cpdb_dcpattributes_poly.shp.csv"
+        },
+        {
+            table: "source_capital_project_pt",
+            columns: [
+                "ccpversion",
+                "maprojid",
+                "magency",
+                "magencyacr",
+                "projectid",
+                "descriptio",
+                "typecatego",
+                "geomsource",
+                "dataname",
+                "datasource",
+                "datadate",
+                "wkt"
+            ],
+            filePath: "data/convert",
+            fileName: "cpdb_dcpattributes_pts.shp.csv"
+        },
+        {
             table: "source_land_use",
             columns: ["id", "description", "color"],
             filePath: "data",
             fileName: "land_use.csv"
         },
-        // {
-        //     table: "source_pluto",
-        //     columns: ["wkt", "borough", "block", "lot", "address", "land_use", "bbl"],
-        //     filePath: "data/download",
-        //     fileName: "pluto.csv"
-        // },
+        {
+            table: "source_pluto",
+            columns: ["wkt", "borough", "block", "lot", "address", "land_use", "bbl"],
+            filePath: "data/download",
+            fileName: "pluto.csv"
+        },
         {
             table: "source_zoning_district",
             columns: ["wkt", "zonedist", "shape_leng", "shape_area"],
             filePath: "data/download",
             fileName: "zoning_districts.csv"
+        },
+        {
+            table: "source_zoning_district_class",
+            columns: ["id", "category", "description", "url", "color"],
+            filePath: "data",
+            fileName: "zoning_district_class.csv"
         }
     ]
 
--- pg/query/source-create.sql
+++ pg/query/source-create.sql
@@ -1,16 +1,20 @@
 DROP TABLE IF EXISTS 
 	source_borough,
 	source_capital_commitment, 
+	source_capital_project,
+	source_capital_project_poly, 
+	source_capital_project_pt,
 	source_city_council_district,
 	source_land_use,
 	source_pluto,
 	source_zoning_district,
+	source_zoning_district_class
 	CASCADE;
 
 CREATE TABLE IF NOT EXISTS source_borough (
-	"id" char(1) PRIMARY KEY NOT NULL,
-	"title" text,
-	"abbr" char(2)
+	id char(1) PRIMARY KEY NOT NULL,
+	title text,
+	abbr char(2)
 );
 
 CREATE TABLE IF NOT EXISTS source_capital_commitment (
@@ -39,32 +43,124 @@ CREATE TABLE IF NOT EXISTS source_capital_commitment (
   plannedcommit_total numeric
 );
 
+CREATE TABLE IF NOT EXISTS  source_capital_project (
+  ccp_version text,
+  m_a_proj_id text,
+  m_agency_acro text,
+  m_agency char(3),
+  m_agency_name text,
+  description text,
+  proj_id text,
+  min_date date,
+  max_date date,
+  type_category text,
+  plannedcommit_ccnonexempt numeric,
+  plannedcommit_ccexempt numeric,
+  plannedcommit_citycost numeric,
+  plannedcommit_nccstate numeric,
+  plannedcommit_nccfederal numeric,
+  plannedcommit_nccother numeric,
+  plannedcommit_noncitycost numeric,
+  plannedcommit_total numeric,
+  adopt_ccnonexempt numeric,
+  adopt_ccexempt numeric,
+  adopt_citycost numeric,
+  adopt_nccstate numeric,
+  adopt_nccfederal numeric,
+  adopt_nccother numeric,
+  adopt_noncitycost numeric,
+  adopt_total numeric,
+  allocate_ccnonexempt numeric,
+  allocate_ccexempt numeric,
+  allocate_citycost numeric,
+  allocate_nccstate numeric,
+  allocate_nccfederal numeric,
+  allocate_nccother numeric,
+  allocate_noncitycost numeric,
+  allocate_total numeric,
+  commit_ccnonexempt numeric,
+  commit_ccexempt numeric,
+  commit_citycost numeric,
+  commit_nccstate numeric,
+  commit_nccfederal numeric,
+  commit_nccother numeric,
+  commit_noncitycost numeric,
+  commit_total numeric,
+  spent_ccnonexempt numeric,
+  spent_ccexempt numeric,
+  spent_citycost numeric,
+  spent_nccstate numeric,
+  spent_nccfederal numeric,
+  spent_nccother numeric,
+  spent_noncitycost numeric,
+  spent_total numeric,
+  spent_total_checkbooknyc numeric
+);
+
+CREATE TABLE IF NOT EXISTS source_capital_project_poly (
+	ccpversion text,
+	maprojid text,
+	magency text,
+	magencyacr text,
+	projectid text,
+	descriptio text,
+	typecatego text,
+	geomsource text,
+	dataname text,
+	datasource text,
+	datadate text,
+	wkt geometry(MULTIPOLYGON, 4326)
+);
+
+CREATE TABLE IF NOT EXISTS source_capital_project_pt (
+	ccpversion text,
+	maprojid text,
+	magency text,
+	magencyacr text,
+	projectid text,
+	descriptio text,
+	typecatego text,
+	geomsource text,
+	dataname text,
+	datasource text,
+	datadate text,
+	wkt geometry(MULTIPOINT, 4326)
+);
+
 CREATE TABLE IF NOT EXISTS source_city_council_district (
-	"coundist" text NOT NULL,
-	"shape_leng" float,
-	"shape_area" float,
-	"wkt" geometry(MULTIPOLYGON, 4326)
+	coundist text NOT NULL,
+	shape_leng float,
+	shape_area float,
+	wkt geometry(MULTIPOLYGON, 4326)
 );
 
 CREATE TABLE IF NOT EXISTS source_land_use (
-	"id" char(2) PRIMARY KEY NOT NULL,
-	"description" text,
-	"color" char(9)
+	id char(2) PRIMARY KEY NOT NULL,
+	description text,
+	color char(9)
 );
 
 CREATE TABLE IF NOT EXISTS source_pluto (
-	"bbl" text PRIMARY KEY NOT NULL,
-	"borough" char(2) NOT NULL,
-	"block" text NOT NULL,
-	"lot" text NOT NULL,
-	"address" text,
-	"land_use" char(2),
-	"wkt" geometry(MULTIPOLYGON, 2263)
+	bbl text PRIMARY KEY NOT NULL,
+	borough char(2) NOT NULL,
+	block text NOT NULL,
+	lot text NOT NULL,
+	address text,
+	land_use char(2),
+	wkt geometry(MULTIPOLYGON, 2263)
 );
 
 CREATE TABLE IF NOT EXISTS source_zoning_district (
-	"zonedist" text NOT NULL,
-	"shape_leng" float,
-	"shape_area" float,
-	"wkt" geometry(MULTIPOLYGON, 4326)
+	zonedist text NOT NULL,
+	shape_leng float,
+	shape_area float,
+	wkt geometry(MULTIPOLYGON, 4326)
 );
+
+CREATE TABLE IF NOT EXISTS source_zoning_district_class (
+	id text PRIMARY KEY,
+	category text,
+	description text,
+	url text,
+	color char(9)
+)
