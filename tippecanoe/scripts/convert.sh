e22c72b2e475dc5851798d4469eff4ebd5e77e7c
tangoyankee <miller.tim108@gmail.com>
2025-05-02 11:23:43 -0400

    z20

--- tippecanoe/scripts/convert.sh
+++ tippecanoe/scripts/convert.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 tippecanoe \
-    -z13 \
+    -z20 \
     --projection=EPSG:4326 \
     -o data/convert/community-district-fill.pmtiles \
     -l community-district-fill data/convert/community-district-fill.json \
@@ -9,14 +9,14 @@ tippecanoe \
 
 tippecanoe \
     -B4 \
-    -z13 \
+    -z20 \
     --projection=EPSG:4326 \
     -o data/convert/community-district-label.pmtiles \
     -l community-district-label data/convert/community-district-label.json \
     --force
 
 tile-join \
-    -o data/convert/community-districts.pmtiles \
+    -o data/convert/community-districts-20.pmtiles \
     data/convert/community-district-fill.pmtiles \
     data/convert/community-district-label.pmtiles \
     --force
