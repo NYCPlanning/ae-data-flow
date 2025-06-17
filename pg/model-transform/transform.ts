b5019fb41cc329a3bcd448e8d347761990917914
pratishta <ypradhi9@gmail.com>
2025-06-17 16:30:29 -0400

    Add way to iterate over graph

--- pg/model-transform/transform.ts
+++ pg/model-transform/transform.ts
@@ -1,5 +1,5 @@
 import { exit } from "process";
-import { Build, buildSchema, buildMap } from "../../schemas";
+import { Build, buildSchema, buildMap, buildArrayExample } from "../../schemas";
 import { pgClient } from "../pg-connector";
 import * as fs from "fs";
 import "dotenv/config";
@@ -36,15 +36,28 @@ import "dotenv/config";
     },
   ];
 
+  const newSources = buildArrayExample;
+  // console.log("new sources", buildMap);
+
+  // this sorts the the sources in order of ascending tree depth, probably so that we're not buildindg 
+  // things that are dependent on others
   const buildSources =
     build === "all"
       ? sources
       : sources.filter((source) => source.builds.includes(build));
+  // console.log("unsorted buildSources", buildSources);
   buildSources.sort((a, b) => a.treeDepth - b.treeDepth);
+  // console.log("sorted build sources", buildSources);
+
+  const newBuildSources = build === "all" ? newSources
+    : newSources.filter((newSource) => newSource.name === build);
+
 
   try {
     await pgClient.connect();
     await pgClient.query("BEGIN;");
+
+    // previous build process
     buildSources.forEach(async (source) => {
       const sql = fs
         .readFileSync(`pg/model-transform/${source.fileName}.sql`)
@@ -52,6 +65,9 @@ import "dotenv/config";
       console.debug("source", source.fileName);
       await pgClient.query(sql);
     });
+
+    // new build process
+    
     await pgClient.query("COMMIT;");
   } catch (e) {
     await pgClient.query("ROLLBACK;");
--- schemas.ts
+++ schemas.ts
@@ -99,4 +99,4 @@ export const buildMap = new Map<Build, BuildDefmap>(
       }
     ],
   ]
-)
+);
