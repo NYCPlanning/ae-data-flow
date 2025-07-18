2f392ce36159380071b3bdb5d96c29c11c7bc167
Timothy Miller <miller.tim108@gmail.com>
2025-07-18 17:03:48 -0400

    create model tables

---
+++ db/pg/model-create/community-board-budget-requests.sh
@@ -0,0 +1,26 @@
+PGPASSWORD=$POSTGRES_PASSWORD \
+psql --host=localhost \
+    --port=5432 \
+    -U $POSTGRES_USER \
+    -d $POSTGRES_DB \
+    --single-transaction \
+    --file ./flow/model-create/community-board-budget-requests.sql
+
+PGPASSWORD=$TARGET_DATABASE_PASSWORD \
+pg_dump --host=$TARGET_DATABASE_HOST  \
+    --port=$TARGET_DATABASE_PORT \
+    -U $TARGET_DATABASE_USER \
+    -d $TARGET_DATABASE_NAME \
+    -s \
+    --no-owner \
+    -t borough \
+    -t community_district \
+    --file ./data/community-board-budget-requests_dump.sql
+
+PGPASSWORD=$POSTGRES_PASSWORD \
+psql --host=localhost \
+    --port=5432 \
+    -U $POSTGRES_USER \
+    -d $POSTGRES_DB \
+    --single-transaction \
+    --file ./data/community-board-budget-requests_dump.sql
