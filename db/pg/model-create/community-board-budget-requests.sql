63421ac3547bf4362cdad8f043c98bf6d9202131
pratishta <ypradhi9@gmail.com>
2025-08-25 16:02:44 -0400

    Initial model tables

--- db/pg/model-create/community-board-budget-requests.sql
+++ db/pg/model-create/community-board-budget-requests.sql
@@ -7,5 +7,29 @@ DROP TABLE IF EXISTS
     cbbr_need_group,
     cbbr_need,
     cbbr_request,
-    cbbr_option_cascade
+    cbbr_option_cascade,
+    community_board_budget_request,
+    agency_response 
 CASCADE;
+
+CREATE TABLE IF NOT EXISTS community_board_budget_request (
+    "id" text NOT NULL,
+    "tracking_code" text NOT NULL,
+    "borough_id" char(1) NOT NULL,
+    "community_district_id" char(2) NOT NULL,
+    "agency" char(3),
+    "agency_category_response" text,
+    "agency_response" text,
+    "type" char(1) NOT NULL,
+    "priority" integer NOT NULL,
+    "need" text,
+    "request" text,
+    "explanation" text,
+    "location_specific" boolean,
+    "geom" geometry(geometry, 4326)
+);
+
+CREATE TABLE IF NOT EXISTS agency_response (
+    "id" char(1) PRIMARY KEY NOT NULL,
+    "description" text NOT NULL
+);
