68aa62b35c034c661bbdbab505095d21ae28efee
Timothy Miller <miller.tim108@gmail.com>
2026-01-12 15:29:15 -0500

    Add borough spatial columns
    Populate zoning database boroughs table with spatial columns
    
    closes #119

--- pg/model-transform/boroughs.sql
+++ pg/model-transform/boroughs.sql
@@ -19,12 +19,18 @@ UPDATE source_borough
 INSERT INTO borough (
     id,
     title,
-    abbr
+    abbr,
+    li_ft,
+    mercator_fill,
+    mercator_label
 )
 SELECT
 	boro_code,
 	boro_name,
-	abbr
+	abbr,
+	ST_Transform(wkt, 2263) AS li_ft,
+    ST_Transform(wkt, 3857) AS mercator_fill,
+    ST_Transform((ST_MaximumInscribedCircle(wkt)).center, 3857) AS mercator_label
 FROM source_borough
 ORDER BY boro_code ASC;
 
