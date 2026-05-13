5090b938783d81e1ee98ccd271402cd2095790f2
pratishta <ypradhi9@gmail.com>
2026-05-13 16:30:37 -0400

    Fill out more transform and populate but multiple null null null rows

--- pg/model-transform/facilities.sql
+++ pg/model-transform/facilities.sql
@@ -2,8 +2,17 @@ TRUNCATE
     facility_operator
     CASCADE;
 
-INSERT INTO facility_operator (abbreviation, name)
+INSERT INTO facility_operator (
+    id,
+    abbreviation, 
+    name, 
+    type
+)
 SELECT DISTINCT
-    OPABBREV as abbreviation,
-    OPNAME as name
-    
+    gen_random_uuid() AS id,
+    "OPABBREV" as abbreviation,
+    "OPNAME" as name,
+    "OPTYPE" as type
+FROM source_facility;
+
+COPY facility_operator TO '/var/lib/postgresql/data/facility_operator.csv';
