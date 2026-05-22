eb948f73f38527a8c74098d98111603023caabde
pratishta <ypradhi9@gmail.com>
2026-05-22 12:01:05 -0400

    Transform and populate facilities options data
    Update source and load step with manually created csvs
    
    Add facility_type
    
    Start transform work
    
    Add facilities categories tables
    
    Work on adding facilities
    
    Add more facilities columns
    
    Finish populate step
    
    Add tables to all
    
    Remove facilities specific code

--- db/pg/target-populate/facilities.sql
+++ db/pg/target-populate/facilities.sql
@@ -1,5 +1,13 @@
 TRUNCATE
-    facility_operator
+    facility_operator,
+    facility_type,
+    facility_subgroup,
+    facility_group,
+    facility_domain
     CASCADE;
 
 \copy facility_operator FROM '/var/lib/postgresql/data/facility_operator.csv';
+\copy facility_domain FROM '/var/lib/postgresql/data/facility_domain.csv';
+\copy facility_group FROM '/var/lib/postgresql/data/facility_group.csv';
+\copy facility_subgroup FROM '/var/lib/postgresql/data/facility_subgroup.csv';
+\copy facility_type FROM '/var/lib/postgresql/data/facility_type.csv';
