84c8f63909aa9a1270afbe58c13f8412037bf227
David Hochbaum <dhochbaum@planning.nyc.gov>
2026-06-29 11:57:44 -0400

    Added URL to data sources

--- pg/model-transform/data-sources.sql
+++ pg/model-transform/data-sources.sql
@@ -6,13 +6,15 @@ INSERT INTO data_source (
     schema_name,
     dataset_name,
     version,
-    retrieve_date
+    retrieve_date,
+    url
 )
 SELECT
     schema_name,
     dataset_name,
     v AS version,
-    archive_date AS retrieve_date
+    archive_date AS retrieve_date,
+    url
 FROM source_data_source;
 
 COPY data_source TO '/var/lib/postgresql/data/data_source.csv';
